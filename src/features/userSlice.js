ab42e4e47889f0341bc0565a926d769cce142d74
ssoura <34943727+ssoura@users.noreply.github.com>
2023-01-05 11:26:20 +0530

    1 file modified

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,21 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
-const axios = require('axios');
-const API_URL = 'https://reqres.in/api/users/';
+import axios from 'axios';
+const API_URL = 'https://reqres.in/api/users';
 
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
-    data: {
-      users: [],
-      userInfo: [],
-    },
+    users: [],
+    userInfo: [],
   },
   reducers: {
     getUserInfo: (state, action) => {
-      state.data.userInfo = [action.payload];
+      state.userInfo = action.payload;
     },
     getUsers: (state, action) => {
-      state.data.users = [action.payload];
+      state.users = action.payload;
     },
   },
 });
@@ -23,7 +21,7 @@ export const userSlice = createSlice({
 export const getUsersAsync = () => async (dispatch) => {
   try {
     const response = await axios.get(`${API_URL}`);
-    dispatch(getUsers(response.data));
+    dispatch(getUsers(response.data.data));
   } catch (err) {
     throw new Error(err);
   }
@@ -32,13 +30,13 @@ export const getUsersAsync = () => async (dispatch) => {
 export const getUserInfoAsync = (data) => async (dispatch) => {
   try {
     const response = await axios.get(`${API_URL}/${data}`);
-    dispatch(getUserInfo(response.data));
+    dispatch(getUserInfo(response.data.data));
   } catch (err) {
     throw new Error(err);
   }
 };
 
 export const { getUsers, getUserInfo } = userSlice.actions;
-export const showUsers = (state) => state.user.data.users;
-export const showUserInfo = (state) => state.user.data.userInfo;
+export const showUsers = (state) => state.user.users;
+export const showUserInfo = (state) => state.user.userInfo;
 export default userSlice.reducer;
